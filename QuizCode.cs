95fe0b00fef2baa0821e0facce54578eac700d74
Joshua Bailey <JBailey@spartaglobal.com>
2020-03-04 14:33:57 +0000

    Second Commit

--- QuizCode.cs
+++ QuizCode.cs
@@ -5,26 +5,5 @@ using System.Threading;
 
 namespace QuizApp2
 {
-    class QuizCode
-    {
-        public int _time;
-        public string _name;
-        public int _scoreCount=0;
-        public int _points;
-        public string Timer()
-        {
-            int i = _time;
-            while(i>0)
-            {
-                Thread.Sleep(1000);
-                i--;
-            }
-            return "Time's Up";
-        }
-        public int Score()
-        {
-            _scoreCount += _points;
-            return _scoreCount;
-        }
-    }
+
 }
